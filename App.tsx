cc90ce960a3683626c999401486c9b2e012b79a5
Oleg Vydrenkov <olegvydrenkov@gmail.com>
2022-12-21 18:11:03 +0200

    feat: Added search bar

--- App.tsx
+++ App.tsx
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Searchbar } from 'react-native-paper';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from 'react-native';
 
 export default function App() {
+  const [search, setSearch] = useState<string>('');
+
+  const handleOnChange = (
+    searchValue: NativeSyntheticEvent<TextInputChangeEventData>,
+  ) => {
+    setSearch(`${searchValue}`);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.searchBarWrapper}>
+        <Searchbar value={search} onChange={handleOnChange} />
+      </View>
+      <View style={styles.listContainer}>
+        <Text>list</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+  },
+  searchContainer: {
+    backgroundColor: 'green',
+    padding: 10,
+  },
+  listContainer: {
+    backgroundColor: 'red',
+    padding: 10,
+    flex: 1,
+  },
+  searchBarWrapper: {
+    padding: 10,
   },
 });
